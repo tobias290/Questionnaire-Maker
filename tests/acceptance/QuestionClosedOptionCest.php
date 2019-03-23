b68842a2bd9ace1f6f8a89952b605e73733ecf40
Toby Essex <tobysx@googlemail.com>
2019-03-23 11:45:35 +0000

    Created add question option tests.

---
+++ tests/acceptance/QuestionClosedOptionCest.php
@@ -0,0 +1,120 @@
+<?php 
+
+class QuestionClosedOptionCest {
+    public function _before(\Step\Acceptance\QuestionnaireMaker $I) {
+        $I->signUp();
+        // And
+        $I->amOnFrontEndPage("dashboard");
+        // And
+        $I->createQuestionnaire();
+        // And
+        $I->seeInCurrentUrl("edit");
+        // And
+        $I->click(["id" => "add-check-box-question"]);
+        // And
+        $I->wait(AcceptanceTester::WAIT_TIME);
+        // And
+        $I->seeInDatabase("question_closed", [
+            "name" => "Untitled",
+        ]);
+        // Then
+        $I->moveMouseOver( ".question-answerable");
+        // And
+        $I->click(["id" => "edit-question"]);
+    }
+
+    /**
+     * Tests to see that an option can be added to a closed question/
+     *
+     * @param \Step\Acceptance\QuestionnaireMaker $I
+     */
+    public function addOption(\Step\Acceptance\QuestionnaireMaker $I) {
+        $I->am("Questionnaire Maker");
+        // And
+        $I->wantTo("add an option to one of my closed questions");
+        // Then
+        $I->click(["id" => "add-question-option"]);
+        // And
+        $I->wait(AcceptanceTester::WAIT_TIME);
+        // And
+        $I->click("Save");
+        // And
+        $I->wait(AcceptanceTester::WAIT_TIME);
+        // And
+        $I->seeInDatabase("question_closed_option", [
+            "option" => "Untitled",
+        ]);
+    }
+
+    /**
+     * Tests to see that an option can be edited on a closed question
+     *
+     * @param \Step\Acceptance\QuestionnaireMaker $I
+     */
+    public function editOption(\Step\Acceptance\QuestionnaireMaker $I) {
+        $I->am("Questionnaire Maker");
+        // And
+        $I->wantTo("edit an option to one of my closed questions");
+        // Then
+        $I->click(["id" => "add-question-option"]);
+        // And
+        $I->wait(AcceptanceTester::WAIT_TIME);
+        // And
+        $I->click("Save");
+        // And
+        $I->wait(AcceptanceTester::WAIT_TIME);
+        // And
+        $I->seeInDatabase("question_closed_option", [
+            "option" => "Untitled",
+        ]);
+        // Then
+        $I->moveMouseOver( ".question-answerable");
+        // And
+        $I->click(["id" => "edit-question"]);
+        // And
+        $I->fillField("option_1", "New Option");
+        // And
+        $I->click("Save");
+        // And
+        $I->seeInDatabase("question_closed_option", [
+            "option" => "New Option",
+        ]);
+    }
+
+    /**
+     * Tests to see that an option can be delete from a closed question
+     *
+     * @param \Step\Acceptance\QuestionnaireMaker $I
+     */
+    public function deleteOption(\Step\Acceptance\QuestionnaireMaker $I) {
+        $I->am("Questionnaire Maker");
+        // And
+        $I->wantTo("delete an option to one of my closed questions");
+        // Then
+        $I->click(["id" => "add-question-option"]);
+        // And
+        $I->wait(AcceptanceTester::WAIT_TIME);
+        // And
+        $I->click("Save");
+        // And
+        $I->wait(AcceptanceTester::WAIT_TIME);
+        // And
+        $I->seeInDatabase("question_closed_option", [
+            "option" => "Untitled",
+        ]);
+        // Then
+        $I->moveMouseOver( ".question-answerable");
+        // And
+        $I->click(["id" => "edit-question"]);
+        // And
+        $I->click(["class" => "delete-question-option"]);
+        // And
+        $I->wait(AcceptanceTester::WAIT_TIME);
+        // And
+        $I->click("Save");
+        // And
+        $I->dontSeeInDatabase("question_closed_option", [
+            "option" => "Untitled",
+        ]);
+    }
+}
---
+++ tests/api/QuestionClosedOptionCest.php
@@ -0,0 +1,138 @@
+<?php 
+
+class QuestionClosedOptionCest {
+    public function _before(\Step\Api\QuestionnaireMaker $I) {
+        $I->signUp();
+        // Then
+        $I->createQuestionnaire();
+        // And
+        $id = $I->getResponse()["success"]["questionnaire_id"];
+        // Then
+        $I->sendPOST("question/add/closed", [
+            "position" => 1,
+            "type" => "radio",
+            "questionnaire_id" => $id,
+        ]);
+        // And
+        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::CREATED);
+        // And
+        $I->seeResponseIsJson();
+        // And
+        $I->seeResponseContainsJson([
+            "success" => [
+                "message" => "Question added"
+            ]
+        ]);
+    }
+
+    /**
+     * Test to see that a HTTP 201 code is returned when the correct data is send to add an option to a question
+     *
+     * @param \Step\Api\QuestionnaireMaker $I
+     */
+    public function addOption(\Step\Api\QuestionnaireMaker $I) {
+        $I->am("Client Side Application");
+        // And
+        $I->wantTo("get the correct response (201) when I send data to add an option to a closed question");
+        // Then
+        $question_id = $I->getResponse()["success"]["id"];
+        // And
+        $I->sendPOST("question/add/closed/option", [
+            "question_closed_id" => $question_id,
+        ]);
+        // And
+        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::CREATED);
+        // And
+        $I->seeResponseIsJson();
+        // And
+        $I->seeResponseContainsJson([
+            "success" => [
+                "message" => "Question option added"
+            ]
+        ]);
+    }
+
+    /**
+     * Test to see that a HTTP 201 code is returned when the correct data is send to edit an option on a question
+     *
+     * @param \Step\Api\QuestionnaireMaker $I
+     */
+    public function editOption(\Step\Api\QuestionnaireMaker $I) {
+        $I->am("Client Side Application");
+        // And
+        $I->wantTo("get the correct response (201) when I send data to edit an option on a closed question");
+        // Then
+        $question_id = $I->getResponse()["success"]["id"];
+        // And
+        $I->sendPOST("question/add/closed/option", [
+            "question_closed_id" => $question_id,
+        ]);
+        // And
+        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::CREATED);
+        // And
+        $I->seeResponseIsJson();
+        // And
+        $I->seeResponseContainsJson([
+            "success" => [
+                "message" => "Question option added"
+            ]
+        ]);
+        // Then
+        $option_id = $I->getResponse()["success"]["id"];
+        // And
+        $I->sendPATCH("question/edit/closed/option/$option_id", [
+            "option" => "Option 1",
+        ]);
+        // And
+        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
+        // And
+        $I->seeResponseIsJson();
+        // And
+        $I->seeResponseContainsJson([
+            "success" => [
+                "message" => "Question option edited"
+            ]
+        ]);
+    }
+
+    /**
+     * Test to see that a HTTP 201 code is returned when the correct data is send to delete an option from a question
+     *
+     * @param \Step\Api\QuestionnaireMaker $I
+     */
+    public function deleteOption(\Step\Api\QuestionnaireMaker $I) {
+        $I->am("Client Side Application");
+        // And
+        $I->wantTo("get the correct response (201) when I send data to delete an option from a closed question");
+        // Then
+        $question_id = $I->getResponse()["success"]["id"];
+        // And
+        $I->sendPOST("question/add/closed/option", [
+            "question_closed_id" => $question_id,
+        ]);
+        // And
+        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::CREATED);
+        // And
+        $I->seeResponseIsJson();
+        // And
+        $I->seeResponseContainsJson([
+            "success" => [
+                "message" => "Question option added"
+            ]
+        ]);
+        // Then
+        $option_id = $I->getResponse()["success"]["id"];
+        // And
+        $I->sendDELETE("question/delete/closed/option/$option_id");
+        // And
+        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
+        // And
+        $I->seeResponseIsJson();
+        // And
+        $I->seeResponseContainsJson([
+            "success" => [
+                "message" => "Question option deleted"
+            ]
+        ]);
+    }
+}
